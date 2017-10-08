a0b06e8405963d5757710dc45cd611a2b0b70797
Adriano Margarin <adriano.margarin@gmail.com>
2017-10-08 16:15:18 -0300

    Speaker

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.shortcuts import resolve_url as r
 
-# Create your models here.
+
+class Speaker(models.Model):
+
+    class Meta:
+        verbose_name = 'Palestrante'
+        verbose_name_plural = 'Palestrantes'
+
+    name = models.CharField(verbose_name='Nome', max_length=255)
+    slug = models.SlugField(verbose_name='Slug')
+    photo = models.URLField(verbose_name='Foto')
+    website = models.URLField(verbose_name='Website', blank=True)
+    description = models.TextField(verbose_name='Descrição', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return r('speaker_detail', slug=self.slug)
--- eventex/urls.py
+++ eventex/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
 from eventex.core.views import home
+from eventex.core.views import speaker_detail
 from eventex.subscriptions.views import detail
 
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
+    url(r'^palestrantes/(?P<slug>[\w-]+)/$', speaker_detail, name='speaker_detail'),
     url(r'^admin/', admin.site.urls),
 ]
